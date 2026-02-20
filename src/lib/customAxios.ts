2bcf445c6850e4f59bfaa236d54ae98ad4dc39c8
Pierson Bosco <pierson@bdgwebdesign.com>
2026-02-20 12:10:34 -0800

    Updated axios settings

--- src/lib/customAxios.ts
+++ src/lib/customAxios.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const customAxios = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
   withXSRFToken: true,
   headers: {
     Accept: "application/json",
